3e0519935b9f75d57f633fe2b6b41f27d8b1c92e
Alex Khmurovich <alexhmurovich@gmail.com>
2024-12-28 16:34:44 -0600

    Initial Commit

---
+++ Alias Word Game/Alias_Word_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Alias_Word_GameApp.swift
+//  Alias Word Game
+//
+//  Created by Alex Khmurovich on 12/28/24.
+//
+
+import SwiftUI
+
+@main
+struct Alias_Word_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
