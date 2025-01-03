081b0d854e530bcde2ab2746e5726ceb8dd73183
Alex Khmurovich <alexhmurovich@gmail.com>
2025-01-03 12:22:46 -0600

    Fix bug with empty window opening, expand words, add new package

--- Alias Word Game/GameSettingsView.swift
+++ Alias Word Game/GameSettingsView.swift
@@ -1,18 +1,18 @@
 //
-//  SettingsView.swift
+//  GameSettingsView.swift
 //  Alias Word Game
 //
-//  Created by Alex Khmurovich on 1/1/25.
+//  Created by Alex Khmurovich on 1/2/25.
 //
 
 import SwiftUI
 
-struct SettingsView: View {
+struct GameSettingsView: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
 #Preview {
-    SettingsView()
+    GameSettingsView()
 }
---
+++ Alias Word Game/Words.swift
@@ -0,0 +1,8 @@
+//
+//  Words.swift
+//  Alias Word Game
+//
+//  Created by Alex Khmurovich on 1/3/25.
+//
+
+import Foundation
